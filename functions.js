c8907f012917e50d1a6816c232fa8def2cb8c690
dsiri12 <dsiri12@gmail.com>
2024-06-19 15:18:05 -0400

    functions added to this new javascript file

---
+++ functions.js
@@ -0,0 +1,26 @@
+
+function myFunction(7, 8) {
+    return 7 * 8;
+}
+
+function myFunction(6, 4, 8) {
+    return 6 + 4 + 8;
+}
+
+let x = myFunction(5, 7);
+
+function myFunction(5, 7) {
+    return 5 * 7;
+}
+
+function myFunction() {
+    let carName = "Honda";
+}
+
+let text = "The temperature is " + toCelcius(65) + " Celcius";
+
+function toCelcius(fahrenheit) {
+    return (5/9) * (fahrenheit-45);
+}
+
+let value = toCelcius(64);
