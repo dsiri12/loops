72d7f3d43b95289a6a27d33825644d8374f8f02b
dsiri12 <dsiri12@gmail.com>
2024-06-21 20:46:15 -0400

    work done functions

--- functionTwo.js
+++ functionTwo.js
@@ -1,5 +1,13 @@
 function add(number1, number2) {
-    console.log(number1 + number2)
+    const sum = number1 + number2
+    console.log(sum)
+    return sum;
 }
 
 add(add(1, 2), multiply(1, 3))
+
+function multiply(number1, number2) {
+    const result = number1 * number2
+      console.log(result)
+      return result;
+}
